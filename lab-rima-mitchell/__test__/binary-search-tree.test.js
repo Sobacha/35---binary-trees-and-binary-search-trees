14c969f66d3b958dcbc5cf518fc0751fb14f3ddd
Rima <rmhrok@gmail.com>
2018-03-03 00:23:43 +0000

    tests for find in bst

--- lab-rima-mitchell/__test__/binary-search-tree.test.js
+++ lab-rima-mitchell/__test__/binary-search-tree.test.js
@@ -54,12 +54,34 @@ describe('BST', () => {
         expect(() => bst.insert()).toThrow('Input needs to be an instanceof TreeNode');
       });
     });
-
     
   });
 
   describe('BST find', () => {
-    
+
+    describe('valid input', () => {
+      test('should return a correct node if value exists', () => {
+        const bst = new BST();
+        const root = new TreeNode(5);
+        const two = new TreeNode(2);
+        const three = new TreeNode(12);
+        const four = new TreeNode(-4);
+        const five = new TreeNode(9);
+        bst.insert(root);
+        bst.insert(two);
+        bst.insert(three);
+        bst.insert(four);
+        bst.insert(five);
+
+        expect(bst.find(-4)).toEqual(four);
+      });
+      test('should return null if value does not exist', () => {
+        const bst = new BST();
+
+        expect(bst.find(-4)).toEqual(null);
+      });
+    });
+
   });
 
   describe('BST findMinInRight', () => {
